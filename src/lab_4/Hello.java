aa0461245d6525f258b6fbf594630f844b7a6fe6
anannyabhatia <anannyabhatia@mc6045-22.labs.sce.carleton.ca>
2018-10-01 12:19:22 -0400

    message one

---
+++ src/lab_4/Hello.java
@@ -0,0 +1,19 @@
+package lab_4;
+
+
+public class Hello {
+
+	public static main(String[] args) 
+	{
+		Hello hello = new Hello();
+		hello.methodA();
+		hello.methodb();
+		hello.methodC();
+	}
+
+	public void methodA(void ) { }
+
+	public void methodB(void) { }
+
+	public void methodC(void) { }
+}
